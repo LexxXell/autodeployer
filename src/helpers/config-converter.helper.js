cd9cd5bfe7d1f10f3743cae2d57151174bae9f3d
Lexx Xell <lexxxell007@gmail.com>
2023-01-28 17:15:27 +0400

    Fixid bug 'exec in not defined' & upgrade config-converter

--- src/helpers/config-converter.helper.js
+++ src/helpers/config-converter.helper.js
@@ -1,3 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+const logger = require('./logger.helper');
+
+function toAbsolute(filePath) {
+  const absolutePath = path.resolve(filePath);
+  if (!fs.existsSync(absolutePath)) {
+    throw new Error(
+      'Unable to convert relative path to absolute!\n' + filePath
+    );
+  }
+  logger('INFO: The config contains a relative path, it is converted to absolute, make sure the conversion is correct:');
+  logger(`└╴${filePath}`)
+  logger(`  └╴${absolutePath}`)
+  return absolutePath;
+}
+
 module.exports = (config) => {
   try {
     if (Array.isArray(config)) {
@@ -6,12 +23,27 @@ module.exports = (config) => {
         const { full_name, ...repositoryConfig } = el;
         result[full_name] = repositoryConfig;
       });
-      return result;
-    } else if (typeof config === 'object') {
-      return config;
+      config = result;
+    } else if (typeof config !== 'object') {
+      throw Error();
     }
-    throw Error;
   } catch {
     throw new Error('Unknown config...');
   }
-}
+  for (full_name of Object.keys(config)) {
+    if (!path.isAbsolute(config[full_name].hook_path)) {
+      config[full_name].hook_path = toAbsolute(config[full_name].hook_path);
+    }
+    if (!path.isAbsolute(config[full_name].project_path)) {
+      config[full_name].project_path = toAbsolute(
+        config[full_name].project_path
+      );
+    }
+    if (!path.isAbsolute(config[full_name].log_dir_path)) {
+      config[full_name].log_dir_path = toAbsolute(
+        config[full_name].log_dir_path
+      );
+    }
+  }
+  return config;
+};
--- src/hook-executor.js
+++ src/hook-executor.js
@@ -1,38 +1,49 @@
+const { exec } = require('node:child_process');
 const { logger } = require('./helpers');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = (repositoryName, repositoryConfig) => {
-  if (!repositoryConfig.hook_path) {
-    logger('No shell-script to execution...')
+  if (
+    !repositoryConfig.hook_path ||
+    !fs.existsSync(repositoryConfig.hook_path)
+  ) {
+    logger('No shell-script to execution...');
     return false;
   }
-  return exec(repositoryConfig.hook_path,
+  return exec(
+    repositoryConfig.hook_path,
     {
       env: {
         ...process.env,
         PROJECT_PATH: repositoryConfig.project_path,
         BRANCH: repositoryConfig.branch,
         CALLBACK: repositoryConfig.callback,
-      }
+      },
     },
     (error, stdout, stderr) => {
       if (error) {
-        throw new Error(error);
+        logger('ERROR: Shellscript hook runtime error.');
       }
-      if (repositoryConfig.log_dir_path) {
+      if (
+        repositoryConfig.log_dir_path &&
+        fs.existsSync(repositoryConfig.log_dir_path)
+      ) {
         fs.writeFileSync(
           path.resolve(
             repositoryConfig.log_dir_path,
             `${repositoryName.replace('/', '.')}_stdout.log`
           ),
-          `${new Date()}\n${stdout}\n`,
+          `${new Date()}\n${stdout}\n`
         );
         fs.writeFileSync(
           path.resolve(
             repositoryConfig.log_dir_path,
             `${repositoryName.replace('/', '.')}_stderr.log`
           ),
-          `${new Date()}\n${stderr}\n`,
-        )
+          `${new Date()}\n${stderr}\n` + error ? error + '\n' : ''
+        );
       }
-    });
-}
+    }
+  );
+};
